4c72fb1abac3b8336f4e3e32174b4a10c964012e
DivyanshuSahu <dsahu1997@gmail.com>
2018-01-21 17:51:40 +0530

    display of books completed

--- downloader.py
+++ downloader.py
@@ -11,9 +11,36 @@ def searchBook(name) :
 	name =name.replace(' ','+')
 	url = ROOT_URL + 'search.php?req=' + str(name) + '&lg_topic=libgen&open=0&view=simple&res=25&phrase=0&column=def'
 	r = requests.get(url)
-	soup = BeautifulSoup(r.text)
-	print soup.prettify()
+	soup = BeautifulSoup(r.text, "lxml")
+	book_lists = soup.find_all("table")[2]
+	
+	# there are total 4 tables inside the page, and the third table is storing the list of book
+	
+	headings = []
+	for h in book_lists.tr.find_all("td") :
+		headings.append(h.text.encode('utf-8'))
+	#print headings
 
+	no_of_available_books = len(book_lists.find_all("tr"))
+	available_books = []
+
+	for i in range(1,no_of_available_books) :
+		available_books.append(book_lists.find_all("tr")[i])
+	# available_books contains list of available books with <tr>
+
+	available_books_details = []
+	for b in available_books :
+		book = []
+		for s in b.find_all("td") :
+			book.append(s.text.encode('utf-8'))
+		available_books_details.append(book)
+	#print available_books_details
+
+	# Display of each book
+	for item in available_books_details :
+		print ""
+		for r in range(8) :
+			print headings[r] + " => " + item[r]
 
 def main() :	
 	parser = optparse.OptionParser()
